3a7c6cc4a4498dc4c41d1fdf4b6447c15419654a
Drew Galloway <2392101G@student.gla.ac.uk>
2020-04-02 22:23:47 +0100

    userpage, list of pubs templates

--- HalfFull/forms.py
+++ HalfFull/forms.py
@@ -1,7 +1,7 @@
 from django import forms 
 from django.contrib.auth.models import User 
 from django.contrib.auth.models import User 
-from HalfFull.models import UserProfile, Pub
+from HalfFull.models import UserProfile, Pub, Crawl
 
 class UserForm(forms.ModelForm): 
     
--- HalfFull/models.py
+++ HalfFull/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username
 
 class Pub(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -17,7 +10,13 @@ class Pub(models.Model):
     atmosphere = models.IntegerField(default=0)
     service = models.IntegerField(default=0)
     picture = models.ImageField(upload_to='pub_images', blank=True)
-	
-		
+    
     def __str__(self): 
         return self.name
+        
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    crawl = models.ManyToManyField(Pub)
+    def __str__(self):
+        return self.user.username
